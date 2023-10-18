9991a8858ed525f3d20a8631b3d2be6cb77aa189
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-10-18 22:30:56 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,14 +3,20 @@ import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_Xv6PI3iLZUZ4pw7opRL1xePIFKo7Q4OkdfELLxbOAzMn5octTXCBJombMv1JP6l7";
 
-export function fetchBreeds() {
-  return axios.get('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.data)
-    .catch(error => console.error(error));
+export async function fetchBreeds() {
+  try {
+        const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+        return response.data;
+    } catch (error) {
+        return console.error(error);
+    }
 }
 
-export function fetchCatByBreed(breedId) {
-  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => response.data[0])
-    .catch(error => console.error(error));
+export async function fetchCatByBreed(breedId) {
+  try {
+        const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
+        return response.data[0];
+    } catch (error) {
+        return console.error(error);
+    }
 }
