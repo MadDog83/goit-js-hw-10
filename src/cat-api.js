d8318d7b3112865fb9897e4cbe4ac1c6eae33d43
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-10-19 21:38:02 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,22 +1,33 @@
-
+// Імпортуємо axios для виконання HTTP-запитів
 import axios from "axios";
 
+// Встановлюємо заголовок x-api-key для всіх запитів
 axios.defaults.headers.common["x-api-key"] = "live_Xv6PI3iLZUZ4pw7opRL1xePIFKo7Q4OkdfELLxbOAzMn5octTXCBJombMv1JP6l7";
 
-export async function fetchBreeds() {
-  try {
-        const response = await axios.get('https://api.thecatapi.com/v1/breeds');
-        return response.data;
-    } catch (error) {
-        return console.error(error);
-    }
+// Функція для отримання масиву порід
+export function fetchBreeds() {
+  // Виконуємо GET-запит на ресурс https://api.thecatapi.com/v1/breeds
+  return axios.get("https://api.thecatapi.com/v1/breeds")
+    .then(response => {
+      // Повертаємо масив порід з відповіді
+      return response.data;
+    })
+    .catch(error => {
+      // Повертаємо помилку у разі відхилення проміса
+      return error;
+    });
 }
 
-export async function fetchCatByBreed(breedId) {
-  try {
-        const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
-        return response.data[0];
-    } catch (error) {
-        return console.error(error);
-    }
+// Функція для отримання даних про кота за ідентифікатором породи
+export function fetchCatByBreed(breedId) {
+  // Виконуємо GET-запит на ресурс https://api.thecatapi.com/v1/images/search з параметром breed_ids
+  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      // Повертаємо дані про кота з відповіді
+      return response.data[0];
+    })
+    .catch(error => {
+      // Повертаємо помилку у разі відхилення проміса
+      return error;
+    });
 }
