ebfb406d60a087acf5a019b9838a11fd446215ee
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-10-20 22:41:44 +0200

    fix

--- src/js/cats-api.js
+++ src/js/cats-api.js
@@ -1,13 +1,16 @@
-// Імпортуємо axios для виконання HTTP-запитів
+
 import axios from "axios";
 
-// Встановлюємо заголовок x-api-key для всіх запитів
+
 axios.defaults.headers.common["x-api-key"] = "live_Xv6PI3iLZUZ4pw7opRL1xePIFKo7Q4OkdfELLxbOAzMn5octTXCBJombMv1JP6l7";
 
+
+axios.defaults.baseURL = "https://api.thecatapi.com/v1";
+
 // Функція для отримання масиву порід
-export function fetchBreeds() {
-  // Виконуємо GET-запит на ресурс https://api.thecatapi.com/v1/breeds
-  return axios.get("https://api.thecatapi.com/v1/breeds")
+function fetchBreeds() {
+  // Виконуємо GET-запит на ресурс /breeds
+  return axios.get("/breeds")
     .then(response => {
       // Повертаємо масив порід з відповіді
       return response.data;
@@ -19,15 +22,18 @@ export function fetchBreeds() {
 }
 
 // Функція для отримання даних про кота за ідентифікатором породи
-export function fetchCatByBreed(breedId) {
-  // Виконуємо GET-запит на ресурс https://api.thecatapi.com/v1/images/search з параметром breed_ids
-  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+function fetchCatByBreed(breedId) {
+  // Виконуємо GET-запит на ресурс /images/search з параметром breed_ids
+  return axios.get(`/images/search?breed_ids=${breedId}`)
     .then(response => {
       // Повертаємо дані про кота з відповіді
-      return response.data[0];
+      return response.data;
     })
     .catch(error => {
       // Повертаємо помилку у разі відхилення проміса
       return error;
     });
 }
+
+// Експортуємо функції fetchBreeds і fetchCatByBreed
+module.exports = { fetchBreeds, fetchCatByBreed };
---
+++ src/js/index.js
@@ -0,0 +1,46 @@
+import { fetchBreeds, fetchCatByBreed } from './cats-api';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+
+const breedSelector = document.querySelector('.breed-select');
+const loadingIndicator = document.querySelector('.loader');
+const catDetails = document.querySelector('.cat-info');
+
+function updateBreedList(breeds) {
+  breedSelector.innerHTML = breeds.map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('\n');
+}
+
+function handleBreedChange(evt) {
+  const selectedBreedId = evt.currentTarget.value;
+  catDetails.classList.add('is-hidden');
+
+  fetchCatByBreed(selectedBreedId)
+    .then(data => {
+      const { breeds, url } = data[0];
+      const { name, temperament, description } = breeds[0];
+
+      catDetails.innerHTML = `
+        <img src="${url}" alt="${name}" width=500>
+        <div class ="back-color">
+          <h2 class="title">${name}</h2>
+          <p class="text">${description}</p>
+          <p class="text span-text"><span class="span">Temperament:</span> ${temperament}</p>
+        </div>`;
+      
+      catDetails.classList.remove('is-hidden');
+    })
+    .catch(() => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', { timeout: 4000, userIcon: false }))
+    .finally(() => loadingIndicator.classList.add('is-hidden'));
+}
+
+function initialize() {
+  fetchBreeds()
+    .then(updateBreedList)
+    .then(() => new SlimSelect({ select: breedSelector }))
+    .catch(() => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', { timeout: 4000, userIcon: false }))
+    .finally(() => loadingIndicator.classList.add('is-hidden'));
+
+  breedSelector.addEventListener('change', handleBreedChange);
+}
+
+initialize();
